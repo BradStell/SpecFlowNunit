ab120cae1b9b92e6aaaa223b98139303d9c3c89a
Brad Stell <brad.stell@kingsmensoftware.com>
2016-04-22 14:46:02 -0400

    First Commit

---
+++ SpecFlow/Features/LoginPage.feature
@@ -0,0 +1,10 @@
+﻿Feature: Login
+	In order to test specflow with nunit
+	as a  ninja developer
+	I need to get specflow tests to run with nunit
+
+@login
+Scenario: Login
+	Given I am on the homepage
+	When I press the Login link
+	Then I should be taken to the login page
---
+++ SpecFlow/Steps/LoginSteps.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium.Chrome;
+
+namespace SpecFlow.Steps
+{
+    [Binding]
+    public class LoginSteps
+    {
+        IWebDriver driver;
+
+        [Given(@"I am on the homepage")]
+        public void GivenIAmOnTheHomepage()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl("https://www.google.com/");
+        }
+        
+        [When(@"I press the Login link")]
+        public void WhenIPressTheLoginLink()
+        {
+            driver.FindElement(By.Id("lst-ib")).SendKeys("Unicorns");
+            
+        }
+        
+        [Then(@"I should be taken to the login page")]
+        public void ThenIShouldBeTakenToTheLoginPage()
+        {
+            driver.FindElement(By.Id("hdtb"));
+            driver.Quit();
+        }
+    }
+}
